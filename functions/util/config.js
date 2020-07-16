89b7e4dffa59b77e0d7d4acd25ae3e4783b4113f
Subhadipta Nayak <52190236+nsubhadipta@users.noreply.github.com>
2020-07-16 15:25:30 +0530

    Update config.js
    hide the original key

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apiKey: "AIzaSyDg6eNMlIQkfgO0w67kHZBgyx4SRgk46B8",
-    authDomain: "nayaxtodoapp.firebaseapp.com",
-    databaseURL: "https://nayaxtodoapp.firebaseio.com",
-    projectId: "nayaxtodoapp",
-    storageBucket: "nayaxtodoapp.appspot.com",
-    messagingSenderId: "762455673560",
-    appId: "1:762455673560:web:6af551066865bf80e025ad",
-    measurementId: "G-GBP38SDJVB"
+    apiKey: "***********************",
+    authDomain: "***********************",
+    databaseURL: "****************",
+    projectId: "****************",
+    storageBucket: "****************",
+    messagingSenderId: "****************",
+    appId: "****************",
+    measurementId: "****************"
   };
